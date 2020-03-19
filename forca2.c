9c3697597a0db953fe652d3b1aabc327219e967e
Brendow Lincoln <linconlbrendow@gmail.com>
2020-03-18 21:39:32 -0300

    Facilitando a leitura do código com funções e introdução ao conceito de ponteiros.

--- forca2.c
+++ forca2.c
@@ -1,11 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-int main() {
+//Função da abertura do jogo da forca
 
-    printf("\nSEJA BEM-VINDO AO JOGO DA FORCA 2000\n\n");
+void  abertura()
+{
 
+ printf("\nSEJA BEM-VINDO AO JOGO DA FORCA 2000\n\n");
+
+}
+
+//Função que pede o chute
+
+void chuta(char chutes[26], int* tentativas)
+{
+    char chute;
+    printf("---------------------------------------------------\n");
+    printf("Qual é a letra?\n");
+    printf("Chute: ");
+    scanf(" %c", &chute);
+    printf("\n\n");
+
+    chutes[*tentativas] = chute;
+    (*tentativas)++;
+}
+
+
+int main()
+{
 
     //Array da palavra secreta 
     
@@ -22,55 +44,58 @@ int main() {
     int tentativas = 0;
     char chutes[26];
 
+    //Chamando a função de inicio
+
+    abertura();
+
     
     //Loop geral do software
 
-    do {
+    do 
+    {
 
-        //Aqui é onde há a interação com o usuário pergunto o chute que ele dá.
-
-        char chute;
-
-        printf("\n");
-        printf("---------------------------------------------------\n");
-        printf("Qual é a letra?\n");
-        printf("Chute: ");
-        scanf(" %c", &chute);
-        printf("\n\n");
-
-        chutes[tentativas] = chute;
-        tentativas++;
-
+        printf("Você já deu %d chutes\n", tentativas);
 
         //Inteligência para identificar se acertou e descobrir a letra.
 
-            for(int i = 0; i < strlen(palavrasecreta); i++){
+        printf("\n");
 
-                int achou =  0;
+        for(int i = 0; i < strlen(palavrasecreta); i++)
+        {
 
-                for(int j =0; j < tentativas; j++) {
+            int achou =  0;
 
-                    if(chutes[j] == palavrasecreta[i]) {
-                        achou = 1;
-                        break;
+            for(int j =0; j < tentativas; j++) 
+            {
 
-                    }
-
-                }
-                
-                if(achou) {
-
-                    printf("%c", palavrasecreta[i]);
-
-                } else {
-
-                    printf("_");
+                if(chutes[j] == palavrasecreta[i]) 
+                {
+                    achou = 1;
+                    break;
 
                 }
 
             }
+            
+            if(achou) 
+            {
 
-            printf("\n");
+                printf("%c", palavrasecreta[i]);
+
+            } else 
+            {
+
+                printf("_ ");
+
+            }
+
+        }
+
+        printf("\n");
+
+        //Aqui é onde é chamado a função para dar o chute.
+
+        chuta(chutes, &tentativas);
         
         
         /*
@@ -81,23 +106,6 @@ int main() {
          */
 
         printf("\n");
-
-        /*Loop de repetição para apresentação da palavra secreta*/
-
-        for(int i = 0; i <= strlen(palavrasecreta); i++) {
-
-            /*
-             *Condicional para verificar se o chute informado bate com alguma letra da
-             *palavra secreta e mostrar qual posição estás.
-             */
-
-            if(palavrasecreta[i] == chute) {
-
-                printf("A posição %d tem essa letra\n", i + 1);
-
-            }
-
-        }
 
         //A "!" (exclamação) nas variáveis a baixo esta invertendo o valor original delas.
 
