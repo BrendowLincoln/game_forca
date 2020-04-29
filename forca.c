63120609ed221050845fabba256f616f36618c02
Brendow Lincoln <linconlbrendow@gmail.com>
2020-04-29 00:16:12 -0300

    Inclusão da função ganhou() e exclusão da variavel acertou.

--- forca.c
+++ forca.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 char palavrasecreta[20];
-int tentativas = 0;
+int chutesdados = 0;
 char chutes[26];
 
 //Função da abertura do jogo da forca
@@ -25,14 +25,14 @@ void chuta()
     scanf(" %c", &chute);
     printf("\n\n");
 
-    chutes[tentativas] = chute;
+    chutes[chutesdados] = chute;
 }
 
 int jachutou(char letra)
 {
     int achou = 0;
 
-    for(int j =0; j < tentativas; j++) 
+    for(int j =0; j < chutesdados; j++) 
     {
 
         if(chutes[j] == letra) 
@@ -47,10 +47,26 @@ int jachutou(char letra)
     return achou;
 }
 
+int ganhou()
+{
+
+    for (int i = 0; i < strlen(palavrasecreta); i++)
+    {
+        if (!jachutou(palavrasecreta[i]))
+        {
+            return 0;
+        }
+        
+    }
+    
+    return 1;
+
+}
+
 void desenhaforca()
 {
 
-    printf("Você já deu %d chutes\n", tentativas);
+    printf("Você já deu %d chutes\n", chutesdados);
 
         //Inteligência para identificar se acertou e descobrir a letra.
 
@@ -87,7 +103,7 @@ int enforcou()
     
     int erros = 0;
 
-    for(int i =0; i < tentativas; i++)
+    for(int i =0; i < chutesdados; i++)
     {
 
         int existe = 0;
@@ -116,16 +132,6 @@ int enforcou()
 
 int main()
 {
- 
-    //Array da palavra secreta 
-    
-    //Vaiáveis que se identificara o término do loop geral e qual motivo.
-
-    int acertou = 0;
-
-    //Armazenamento dos chutes dados para o loop de aparecer e npumero de tentativas
-
-    //Chamando a função de inicio
 
     abertura();
     escolherpalavra();
@@ -139,21 +145,11 @@ int main()
         //Aqui é onde é chamado a função para dar o chute.
 
         chuta();
-        tentativas++;
+        chutesdados++;
         
-        /*
-         *BUG DO SCANF: O scanf reconhece o enter como um caractere e quando validamos um chute,
-         *o enter fica como BUFF. Então quando ele nos pergunta de novo, o BUFF é validado na hora
-         *rodando mais uma vez sem nos perguntar. Por isso é preciso adicionar um espaço dentro das
-         *aspas antes da máscara
-         */
 
         printf("\n");
 
-        //A "!" (exclamação) nas variáveis a baixo esta invertendo o valor original delas.
-
-        //Transformou o valor da variável de 0 para 1. Então, o loop
-
-    } while (!acertou && !enforcou());
+    } while (!ganhou() && !enforcou());
 
 }
